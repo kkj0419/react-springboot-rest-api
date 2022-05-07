91b559063e50fd5f3dad0a5555c5a0b8a9b57c65
kkj0419 <hgkimmme9@gmail.com>
2022-05-07 14:39:25 +0900

    OrderService

---
+++ gc-coffe/src/main/java/com/example/gccoffe/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.example.gccoffe.service;
+
+import com.example.gccoffe.model.Email;
+import com.example.gccoffe.model.Order;
+import com.example.gccoffe.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderService {
+
+    Order creatOrder(Email email, String address, String postcode, List<OrderItem> orderItems);
+}
