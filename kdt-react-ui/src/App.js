214e20881909cd0e02866434001f181b6afea899
kkj0419 <hgkimmme9@gmail.com>
2022-05-07 15:08:20 +0900

    react App 추가

---
+++ kdt-react-ui/src/App.js
@@ -0,0 +1,74 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import React, {useEffect, useState} from "react";
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+import axios from "axios";
+
+
+function App() {
+    const [products, setProducts] = useState([
+        {productId: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: 3000},
+        {productId: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: 4000},
+        {productId: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 5000}
+    ]);
+
+    const [items, setItems] = useState([]);
+
+    const handleAddClicked = productId => {
+        const product = products.find(v => v.productId === productId);
+        const found = items.find(v => v.productId === productId);
+        const updatedItems =
+            found ? items.map(v => (v.productId === productId) ? {
+                ...v,
+                count: v.count + 1
+            } : v) : [...items, {...product, count: 1}];
+        setItems(updatedItems);
+    }
+
+    //TODO DOM 준비 완료 시 호출(AXIOS)
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/v1/products")
+            .then(v => setProducts(v.data));
+    }, [])
+
+    const handleOrderSubmit = (order) => {
+        if (items.length === 0) {
+            axios.post('http://localhost:8080/api/v1/orders', {
+                email: order.email,
+                address: order.address,
+                postcode: order.postcode,
+                orderItems:
+                    items.map(v => ({
+                        productId: v.productId,
+                        category: v.category,
+                        price: v.price,
+                        quantity: v.count
+                    }))
+            }).then(v => alert('주문이 정상적으로 접수되었습니다.'), e => {
+                alert("error");
+                console.error(e);
+            })
+        }
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-center m-4">
+                <h1 className="text-center">Grids & Circle</h1>
+            </div>
+            <div className="card">
+                <div className="row">
+                    <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+                        <ProductList products={products} onAddClick={handleAddClicked}/>
+                    </div>
+                    <div className="col-md-4 summary p-4">
+                        <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
